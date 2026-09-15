43c25a4aeaa6d15083b71313c6454eea21b24cc4
hassaku918 <hassaku918@gmail.com>
2026-09-15 09:18:54 +0900

    Fix Android SDK license acceptance in CI workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,21 @@ android {
         versionCode = 3
         versionName = "3.0"
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
 }
 
 dependencies {
